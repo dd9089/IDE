d9d485599d8b78c53a9c9ea0a2e63358d8603057
Noah Bhuiya (RIT Student) <njb9502@rit.edu>
2025-02-21 09:48:14 -0500

    Added LED, Switch, UART init files

---
+++ LED.h
@@ -0,0 +1,21 @@
+/*
+ * File:        LED.h
+ * Purpose:     Provide LED routines for serial IO
+ *
+ * Notes:		
+ *
+ */
+#include "Common.h"  // from Lab1 code
+
+void LED1_Init(void);
+	
+void LED1_On(void);
+
+void LED1_Off(void);
+
+
+void LED2_Init(void);
+
+void LED2_On(int);
+	
+void LED2_Off(void);

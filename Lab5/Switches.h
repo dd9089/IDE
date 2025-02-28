66d23b20080389ddda2db00ed0cf290ac386e064
dd9089 <103137009+dd9089@users.noreply.github.com>
2025-02-27 23:17:50 -0500

    Add files via upload

---
+++ Lab5/Switches.h
@@ -0,0 +1,15 @@
+/*
+ * File:        Switches.h
+ * Purpose:     Provide routines for switch configuration and use
+ *
+ * Notes:		
+ *
+ */
+ 
+#include "Common.h"
+ 
+void Switch1_Init(void);
+void Switch2_Init(void);
+	 
+BOOLEAN Switch1_Pressed(void);
+BOOLEAN Switch2_Pressed(void);
